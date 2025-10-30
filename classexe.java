30834320395f823b0b3007ab07450a71202be581
yigitcer04 <143340313+yigitcer04@users.noreply.github.com>
2025-10-30 16:23:07 -0400

    Implement interface for operations with demo methods

---
+++ classexe.java
@@ -0,0 +1,21 @@
+interface Operation {
+    int compute(int a, int b);
+}
+
+public class Main {
+    public static void main(String[] args) {
+        // Anonymous inner class for multiplication
+        Operation multiply = new Operation() {
+            @Override
+            public int compute(int a, int b) {
+                return a * b;
+            }
+          interfaceDemo1(add, 4,5);
+          interfaceDemo2(mult, 4,5);
+        };
+
+      public static void interfaceDemo1 (Operation, obj, int x, int y) {
+        System.out.println("The operation result is:" * obj.compute(x,y)
+        
+    }
+}
